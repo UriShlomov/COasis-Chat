b7146141730e83a49d83b3ff84e5d21469ab427f
UriShlomov <sl.uriel@gmail.com>
2019-05-26 00:40:53 +0300

    chat

---
+++ index.js
@@ -0,0 +1,32 @@
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+app.get('/', function(request, respons) {
+	respons.sendFile(__dirname + '/index.html');
+});
+
+connections = [];
+
+io.sockets.on('connection', function(socket) {
+	console.log("Успешное соединение");
+	
+	connections.push(socket);
+	
+	socket.on('disconnect', function(data) {
+		
+		connections.splice(connections.indexOf(socket), 1);
+		console.log("Отключились");
+	});
+
+	
+	socket.on('send mess', function(data) {
+		
+		io.sockets.emit('add mess', {mess: data.mess, name: data.name, className: data.className});
+	});
+
+});
